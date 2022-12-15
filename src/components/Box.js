3818b0ef7185c4e3bbd4fe00a5928885272ec95e
claire <yue.katou@gmail.com>
2022-12-15 12:18:42 -0800

    removes axios, useEffect hook

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,29 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import TabList from "./TabList";
 import Card from "./Card";
+import recipeList from "./../data/recipes";
 
 const Box = () => {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState(recipeList);
   const [selectedRecipe, setSelectedRecipe] = useState({});
 
   const selectTab = (recipe) => {
     setSelectedRecipe(recipe);
   };
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.spoonacular.com/recipes/random?apiKey=e9e571deee31402faeed1d453f7cd52f&number=8"
-      )
-      .then((response) => {
-        setRecipes(response.data.recipes);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
-    <div className="box container bg-info py-2">
+    <div className='box container bg-info py-2'>
       <TabList allRecipes={recipes} selectTab={selectTab} />
       <Card selectedRecipe={selectedRecipe} />
     </div>
